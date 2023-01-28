e86d55041468730ef879b9d778333929f775803a
kittii9797 <73033718+kittii9797@users.noreply.github.com>
2023-01-28 18:10:12 +0100

    update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,8 +9,6 @@
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
 
-        <!-- Styles -->
-        <link href="{{ asset('css/normalize.css') }}" rel="stylesheet"> 
 
     </head>
     <body class="antialiased">
